7557ac7b55cf0d9dd9d8d760a515da711904a506
Serena Sensini <serena.sensini@gmail.com>
2020-01-15 18:50:07 +0100

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Camera } from '@ionic-native/camera/ngx';
+import {Base64ToGallery} from '@ionic-native/base64-to-gallery/ngx';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,31 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  foto: any;
+
+  constructor(private camera: Camera, private base64ToGallery: Base64ToGallery) {}
+
+  scattaFoto(): void {
+
+    const options = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      sourceType: this.camera.PictureSourceType.CAMERA,
+      encodingType: this.camera.EncodingType.JPEG,
+      targetWidth: 150,
+      targetHeight: 150,
+      saveToPhotoAlbum: true
+    };
+
+    this.camera.getPicture(options).then((imagePath) => {
+      this.foto = 'data:image/jpeg;base64,' + imagePath;
+      this.base64ToGallery.base64ToGallery(this.foto).then(
+          res => console.log('Immagine salvata nella galleria ', res),
+          err => console.log('Errore durante il salvataggio dell\'immagine.', err)
+      );
+    }, (err) => {
+      console.log('Errore durante il caricamento dell\'immagine.');
+    });
+  }
 
 }
